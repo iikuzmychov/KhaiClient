ebe1bceb1c645d48ce7f1c34c23e9d9201f05777
kuzcode <igormakw228@gmail.com>
2022-11-07 00:30:16 +0200

    Rename KhaiApiClient to KhaiClient and finish GetStudentWeekSheduleAsync method

---
+++ KhaiApiClient/EnumerableExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KhaiApiClient;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<TSource[]> SplitBy<TSource>(
+        this IEnumerable<TSource> source, TSource[] separators, IEqualityComparer<TSource>? comparer = null)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(separators);
+        
+        return SplitByIterator(source, separators, comparer);
+    }
+
+    private static IEnumerable<TSource[]> SplitByIterator<TSource>(
+        this IEnumerable<TSource> source, TSource[] separators, IEqualityComparer<TSource>? comparer)
+    {
+        comparer ??= EqualityComparer<TSource>.Default;
+
+        var current = new List<TSource>();
+
+        foreach (var item in source)
+        {
+            if (separators.Contains(item, comparer))
+            {
+                if (current.Count > 0)
+                {
+                    yield return current.ToArray();
+                    current = new List<TSource>();
+                }
+            }
+            else
+            {
+                current.Add(item);
+            }
+        }
+
+        if (current.Count > 0)
+            yield return current.ToArray();
+    }
+}
--- KhaiApiClient/KhaiClient.cs
+++ KhaiApiClient/KhaiClient.cs
@@ -1,4 +1,9 @@
-﻿using System.Net.Http;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace KhaiApiClient;
 
@@ -6,15 +11,100 @@ public class KhaiClient
 {
     private const string BaseUrl = "https://education.khai.edu/";
 
-    private HttpClient _httpClient;
+    private readonly HttpClient _httpClient;
 
-    public KhaiApiClient()
+    public KhaiClient()
     {
         _httpClient = new HttpClient()
         {
-            BaseAddress = BaseUrl
+            BaseAddress = new(BaseUrl)
         };
     }
 
-    public  Get
+    public async Task<WeekSchedule> GetStudentWeekSheduleAsync(string studentId)
+    {
+        ArgumentNullException.ThrowIfNull(studentId);
+
+        if (string.IsNullOrWhiteSpace(studentId))
+            throw new ArgumentException("A student ID is an empty string.", nameof(studentId));
+
+        var response      = (await _httpClient.GetAsync(@$"union/schedule/student/{studentId}")).EnsureSuccessStatusCode();
+        var contentStream = await response.Content.ReadAsStreamAsync();
+        
+        var html = new HtmlDocument();
+        html.Load(contentStream);
+
+        var scheduleTable = html.DocumentNode.SelectSingleNode("//div[@class='mud-table-container']/table");
+
+        return ParseWeekSchedule(scheduleTable);
+    }
+
+    private WeekSchedule ParseWeekSchedule(HtmlNode scheduleTable)
+    {
+        var scheduleRows = scheduleTable.SelectNodes("tr");
+
+        var headerRows = scheduleRows
+            .Where(row =>
+                row.FirstChild.Name == "th" &&
+                row.FirstChild.Attributes.Contains("colspan") &&
+                row.FirstChild.Attributes["colspan"].Value == "3")
+            .ToArray();
+
+        var rowsByDay    = scheduleRows.SplitBy(headerRows);
+        var daySchedules = new List<DaySchedule>();
+
+        foreach (var rows in rowsByDay)
+        {
+            var dayClasses = new List<AlternateUniversityClass>();
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                var numeratorClassCells = rows[i].Descendants("td").ToArray();
+                var numeratorClass      = ParseUniversityClass(numeratorClassCells[1].GetDirectInnerText());
+                
+                UniversityClass? denominatorClass;
+
+                if (numeratorClassCells[0].Attributes.Contains("rowspan"))
+                {
+                    var denominatorClassCells = rows[i + 1].Descendants("th");
+                    denominatorClass = ParseUniversityClass(denominatorClassCells.First().GetDirectInnerText());
+
+                    i++;
+                }
+                else
+                {
+                    denominatorClass = numeratorClass;
+                }
+
+                dayClasses.Add(new AlternateUniversityClass(numeratorClass, denominatorClass));
+            }
+
+            daySchedules.Add(DaySchedule.Parse(dayClasses));
+        }
+
+        return WeekSchedule.Parse(daySchedules);
+    }
+
+    private UniversityClass? ParseUniversityClass(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            return null;
+
+        var items = text.Split(',');
+        string name;
+        string? roomNumber;
+
+        if (char.IsDigit(items[0][0]))
+        {
+            name       = items[1];
+            roomNumber = items[0];
+        }
+        else
+        {
+            name       = items[0];
+            roomNumber = null;
+        }
+
+        return new UniversityClass(name, roomNumber);
+    }
 }
