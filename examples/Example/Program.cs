76a6e6b773b6fa143a94b0fc127f5050c7e0faec
Ihor Kuzmychov <37931581+iiKuzmychov@users.noreply.github.com>
2022-11-07 17:08:59 +0200

    Add URL in example

--- examples/Example/Program.cs
+++ examples/Example/Program.cs
@@ -1,10 +1,15 @@
-﻿using Khai;
+using Khai;
 using System.Diagnostics;
 
 using var client = new KhaiClient();
 
-var studentSchedule  = await client.GetStudentWeekSheduleAsync("kuzmichov-i-i");
+// https://education.khai.edu/union/schedule/student/kuzmichov-i-i
+var studentSchedule = await client.GetStudentWeekSheduleAsync("kuzmichov-i-i");
+
+// https://education.khai.edu/union/schedule/lecturer/abramov-k-d-504
 var lecturerSchedule = await client.GetLecturerWeekSheduleAsync("abramov-k-d-504");
+
+// https://education.khai.edu/union/schedule/group/623p
 var groupSchedule    = await client.GetGroupWeekSheduleAsync("623p");
 
 Debugger.Break();
