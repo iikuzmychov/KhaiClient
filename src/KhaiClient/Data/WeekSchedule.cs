f594c539e6e2b337857266a3109882bcc28b0ca4
kuzcode <igormakw228@gmail.com>
2022-12-18 18:21:49 +0200

    Refactor WeekSchedule

--- src/KhaiClient/Data/WeekSchedule.cs
+++ src/KhaiClient/Data/WeekSchedule.cs
@@ -1,80 +1,63 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Khai;
 
-public class WeekSchedule : IEnumerable<DaySchedule>
+public class WeekSchedule
 {
-    private DaySchedule _monday = new();
-    private DaySchedule _tuesday = new();
-    private DaySchedule _wednesday = new();
-    private DaySchedule _thursday = new();
-    private DaySchedule _friday = new();
+    private DaySchedule[] _days;
 
     public DaySchedule Monday
     {
-        get => _monday;
-        set => _monday = value ?? throw new ArgumentNullException(nameof(value));
+        get => _days[0];
+        set => _days[0] = value ?? throw new ArgumentNullException(nameof(value));
     }
     public DaySchedule Tuesday
     {
-        get => _tuesday;
-        set => _tuesday = value ?? throw new ArgumentNullException(nameof(value));
+        get => _days[1];
+        set => _days[1] = value ?? throw new ArgumentNullException(nameof(value));
     }
     public DaySchedule Wednesday
     {
-        get => _wednesday;
-        set => _wednesday = value ?? throw new ArgumentNullException(nameof(value));
+        get => _days[2];
+        set => _days[2] = value ?? throw new ArgumentNullException(nameof(value));
     }
     public DaySchedule Thursday
     {
-        get => _thursday;
-        set => _thursday = value ?? throw new ArgumentNullException(nameof(value));
+        get => _days[3];
+        set => _days[3] = value ?? throw new ArgumentNullException(nameof(value));
     }
     public DaySchedule Friday
     {
-        get => _friday;
-        set => _friday = value ?? throw new ArgumentNullException(nameof(value));
+        get => _days[4];
+        set => _days[4] = value ?? throw new ArgumentNullException(nameof(value));
     }
 
-    public WeekSchedule() { }
+    public WeekSchedule() : this(new(), new(), new(), new(), new()) { }
 
     public WeekSchedule(DaySchedule monday, DaySchedule tuesday,
         DaySchedule wednesday, DaySchedule thursday, DaySchedule friday)
-    { 
-        Monday    = monday;
-        Tuesday   = tuesday;
-        Wednesday = wednesday;
-        Thursday  = thursday;
-        Friday    = friday;
-    }
-
-    public static WeekSchedule Parse(IList<DaySchedule> daySchedules)
     {
-        ArgumentNullException.ThrowIfNull(daySchedules);
+        ArgumentNullException.ThrowIfNull(monday);
+        ArgumentNullException.ThrowIfNull(tuesday);
+        ArgumentNullException.ThrowIfNull(wednesday);
+        ArgumentNullException.ThrowIfNull(thursday);
+        ArgumentNullException.ThrowIfNull(friday);
 
-        if (daySchedules.Count != 5)
-            throw new ArgumentException("A day schedules count must equals 5.");
-
-        return new WeekSchedule(daySchedules[0], daySchedules[1], daySchedules[2], daySchedules[3], daySchedules[4]);
+        _days = new DaySchedule[5] { monday, tuesday, wednesday, thursday, friday };
+        Days  = new ReadOnlyCollection<DaySchedule>(_days);
     }
 
-    IEnumerator<DaySchedule> IEnumerable<DaySchedule>.GetEnumerator()
+    public static WeekSchedule Parse(IList<DaySchedule> days)
     {
-        yield return Monday;
-        yield return Tuesday;
-        yield return Wednesday;
-        yield return Thursday;
-        yield return Friday;
+        ArgumentNullException.ThrowIfNull(days);
+
+        if (days.Count != 5)
+            throw new ArgumentException("The days count must equals 5.");
+
+        return new WeekSchedule(days[0], days[1], days[2], days[3], days[4]);
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        yield return Monday;
-        yield return Tuesday;
-        yield return Wednesday;
-        yield return Thursday;
-        yield return Friday;
-    }
+    public IList<DaySchedule> AsDays() => new ReadOnlyCollection<DaySchedule>(_days);
 }
